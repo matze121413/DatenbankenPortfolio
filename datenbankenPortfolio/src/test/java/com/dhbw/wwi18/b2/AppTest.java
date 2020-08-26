33d4ea79975b6fa95639efa5ecbf3274340e0c6d
Matthias Schultze-Westrum <matthias.schultze-westrum@dm.de>
2020-08-26 07:40:10 +0200

    initial commit

---
+++ datenbankenPortfolio/src/test/java/com/dhbw/wwi18/b2/AppTest.java
@@ -0,0 +1,20 @@
+package com.dhbw.wwi18.b2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
