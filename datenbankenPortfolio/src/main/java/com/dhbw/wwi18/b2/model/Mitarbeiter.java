e910f8b7d489c7d62260aa2e2ff9e9cff1147548
Matthias Schultze-Westrum <matthias.schultze-westrum@dm.de>
2020-10-07 16:59:15 +0200

    flyway als dependency/ plugin hinzugefügt, um Datenbakmigrationen zu ermöglichen. Erstellen eines ersten Datenbankschemas mit Entity Mitarbeiter

---
+++ datenbankenPortfolio/src/main/java/com/dhbw/wwi18/b2/model/Mitarbeiter.java
@@ -0,0 +1,10 @@
+package com.dhbw.wwi18.b2.model;
+
+import lombok.Data;
+
+@Data
+public class Mitarbeiter {
+    Long mitarbeiterId;
+    String name;
+    Integer alter;
+}
---
+++ datenbankenPortfolio/src/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `mitarbeiter` (
+ `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ `name` char(255),
+ `alter` int
+);
